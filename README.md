c71a99649aa51b40306fa1682816caaefd27079f
Julián Albarellos <53489408+JulianAlbarellos@users.noreply.github.com>
2023-10-11 12:04:28 -0300

    Show after set

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # GeneXus-KBBuilder-Shared-Library
 GeneXus shared library for Jenkins
+
+
+
+
+Plugins:
+  - Pipeline Utility Steps (readProoperties)
+  - GeneXus Plugin
+  - MSBuild Plugin
--- vars/readPipelineProperties.groovy
+++ vars/readPipelineProperties.groovy
@@ -20,23 +20,23 @@ def call(Map args = [:]) {
     props = readProperties defaults: props, file: args.machineFilePath;
     String msbuildExePath = powershell script: "Join-Path \"${props.msbuildInstallationPath}\" \"msbuild.exe\"", returnStdout: true
     props.msbuildExePath = msbuildExePath.trim()
-    echo "INFO MSBuild:: ${msbuildExePath.trim()}"
+    echo "INFO MSBuild:: ${props.msbuildExePath}"
 
     String localGXPath = powershell script: "[System.IO.Path]::GetFullPath(\"${WORKSPACE}\\..\\genexus\")", returnStdout: true
     props.localGXPath = localGXPath.trim()
-    echo "INFO GeneXus Installation:: ${localGXPath.trim()}"
+    echo "INFO GeneXus Installation:: ${props.localGXPath}"
     if(props.genexusNeedAndroidSDK) {
         String localAndroidSDKPath = powershell script: "[System.IO.Path]::GetFullPath(\"${WORKSPACE}\\..\\androidSDK\")", returnStdout: true
         props.localAndroidSDKPath = localAndroidSDKPath.trim()
-        echo "INFO AndroidSDK:: ${localAndroidSDKPath.trim()}"
+        echo "INFO AndroidSDK:: ${props.localAndroidSDKPath}"
     }
     String localKBPath = powershell script: "[System.IO.Path]::GetFullPath(\"${WORKSPACE}\\..\\kb\\${props.gxserverKB}\")", returnStdout: true
     props.localKBPath = localKBPath.trim()
-    echo "INFO KnowledgeBase:: ${localKBPath.trim()}"
+    echo "INFO KnowledgeBase:: ${props.localKBPath}"
 
     String localUnitTestingPath = powershell script: "[System.IO.Path]::GetFullPath(\"${WORKSPACE}\\..\\tests\\unit\")", returnStdout: true
     props.localUnitTestPath = localUnitTestingPath.trim()
-    echo "INFO localUnitTestPath:: ${localUnitTestingPath.trim()}"
+    echo "INFO localUnitTestPath:: ${props.localUnitTestPath}"
 
     return props
 }
