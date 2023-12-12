21f6ef0b12707e7ceeba034db8e7bc335141ed1c
Julián Albarellos <53489408+JulianAlbarellos@users.noreply.github.com>
2023-12-12 14:27:51 -0300

    Add module update method

---
+++ vars/updateInstalledModule.groovy
@@ -0,0 +1,26 @@
+/*
+ * Job checkPendingReorg >> Read properties from environment
+ *
+ * @Param args = [:]
+ * +- localGXPath
+ * +- localKBPath
+ * +- environmentName
+ * +- propertiesFilePath
+ * +- machineFilePath
+ */
+
+def call(Map args = [:]) {
+    // Sync properties.msbuild
+    def fileContents = libraryResource 'com/genexus/templates/cdxci.msbuild'
+    writeFile file: 'cdxci.msbuild', text: fileContents
+
+    bat label: 'Build all', 
+    script: """
+        "${args.msbuildExePath}" "${WORKSPACE}\\cdxci.msbuild" \
+        /p:GX_PROGRAM_DIR="${args.localGXPath}" \
+        /p:localKbPath="${args.localKBPath}" \
+        /p:environmentName="${args.environmentName}" \
+        /p:moduleName="${args.moduleName}" \
+        /t:UpdateInstalledModule
+    """
+}
