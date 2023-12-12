fbdf829d2f79a3fd24c19395bab1bc25385c308e
Julián Albarellos <53489408+JulianAlbarellos@users.noreply.github.com>
2023-12-12 15:32:25 -0300

    Add Close KB method 2

--- vars/closeKnowledgeBase.groovy
+++ vars/closeKnowledgeBase.groovy
@@ -10,15 +10,17 @@
  */
 
 def call(Map args = [:]) {
-    // Sync properties.msbuild
-    def fileContents = libraryResource 'com/genexus/templates/cdxci.msbuild'
-    writeFile file: 'cdxci.msbuild', text: fileContents
-    
-    bat label: "Close Knowledge Base",
-        script: """
-            "${args.msbuildExePath}" "${WORKSPACE}\\cdxci.msbuild" \
-            /p:GX_PROGRAM_DIR="${args.localGXPath}" \
-            /p:localKbPath="${args.localKBPath}" \
-            /t:CloseKB
-        """
+    if(args.localKBPath != null) {
+        // Sync properties.msbuild
+        def fileContents = libraryResource 'com/genexus/templates/cdxci.msbuild'
+        writeFile file: 'cdxci.msbuild', text: fileContents
+        
+        bat label: "Close Knowledge Base",
+            script: """
+                "${args.msbuildExePath}" "${WORKSPACE}\\cdxci.msbuild" \
+                /p:GX_PROGRAM_DIR="${args.localGXPath}" \
+                /p:localKbPath="${args.localKBPath}" \
+                /t:CloseKB
+            """
+    }
 }
