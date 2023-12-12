de6a55ed9f68683d14f1e1140f295d2455e11c9c
Julián Albarellos <53489408+JulianAlbarellos@users.noreply.github.com>
2023-12-12 12:06:20 -0300

    Add build all n avoid reorganization meethods

---
+++ vars/buildWithoutReorg.groovy
@@ -0,0 +1,40 @@
+/*
+ * Job buildKnowledgeBase >> Read properties from environment
+ *
+ * @Param args = [:]
+ * +- localGXPath
+ * +- localKBPath
+ * +- environmentName
+ * +- propertiesFilePath
+ * +- machineFilePath
+ */
+
+def call(Map args = [:]) {
+
+    // Sync properties.msbuild
+    def fileContents = libraryResource 'com/genexus/templates/cdxci.msbuild'
+    writeFile file: 'cdxci.msbuild', text: fileContents
+
+    
+    def fileContents = libraryResource 'com/genexus/templates/properties.msbuild'
+    writeFile file: 'properties.msbuild', text: fileContents
+
+    bat label: 'Avoid Datastore connections', 
+        script: """
+            "${args.msbuildExePath}" "${WORKSPACE}\\properties.msbuild" \
+            /p:GX_PROGRAM_DIR="${args.localGXPath}" \
+            /p:localKbPath="${args.localKBPath}" \
+            /p:environmentName="${args.environmentName}" \
+            /t:AvoidDatastoreConnections
+        """
+
+    bat label: 'Build all', 
+        script: """
+            "${args.msbuildExePath}" "${WORKSPACE}\\cdxci.msbuild" \
+            /p:GX_PROGRAM_DIR="${args.localGXPath}" \
+            /p:localKbPath="${args.localKBPath}" \
+            /p:environmentName="${args.environmentName}" \
+            /p:rebuild="${args.forceRebuild}" \
+            /t:BuildEnvironment
+        """
+}
