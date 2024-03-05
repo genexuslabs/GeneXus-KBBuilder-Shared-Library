caece544328928c3a2918d707c73398fe1845175
Julián Albarellos <53489408+JulianAlbarellos@users.noreply.github.com>
2024-03-05 09:43:16 -0300

    Fix method signature 2

--- src/com/genexus/PropertiesHelper.groovy
+++ src/com/genexus/PropertiesHelper.groovy
@@ -4,7 +4,7 @@ package com.genexus
  * This method set generator property using msbuild command
  * @param 
  */
-void setGeneratorProperty(Map args = [:]) { // String msbuildExePath, String gxBasePath, String localKBPath, String environmentName, String generatorName, String propName, String propValue) {
+void setGeneratorProperty(Map args = [:], String generatorName, String propName, String propValue) {
     try {
         // Sync properties.msbuild -- TODO no sync if exists
         fileContents = libraryResource 'com/genexus/templates/properties.msbuild'
@@ -15,9 +15,9 @@ void setGeneratorProperty(Map args = [:]) { // String msbuildExePath, String gxB
                 /p:GX_PROGRAM_DIR="${args.gxBasePath}" \
                 /p:localKbPath="${args.localKBPath}" \
                 /p:environmentName="${args.environmentName}" \
-                /p:generatorName="${args.generatorName}" \
-                /p:propertyName="${args.propName}" \
-                /p:propertyValue="${args.propValue}" \
+                /p:generatorName="${generatorName}" \
+                /p:propertyName="${propName}" \
+                /p:propertyValue="${propValue}" \
                 /t:SetGeneratorProperty
             """
     } catch (error) {
