af46fea1a1775971305ee6e343822553bab7f603
Julián Albarellos <53489408+JulianAlbarellos@users.noreply.github.com>
2024-03-05 09:01:37 -0300

    Add properties helper

---
+++ src/com/genexus/PropertiesHelper.groovy
@@ -0,0 +1,28 @@
+package com.genexus
+
+/**
+ * This method set generator property using msbuild command
+ * @param 
+ */
+void setGeneratorProperty(String msbuildExePath, String gxBasePath, String localKBPath, String generatorName, String propName, String propValue) {
+    try {
+        // Sync properties.msbuild -- TODO no sync if exists
+        fileContents = libraryResource 'com/genexus/templates/properties.msbuild'
+        writeFile file: 'properties.msbuild', text: fileContents
+
+        bat script: """
+                "${msbuildExePath}" "${WORKSPACE}\\properties.msbuild" \
+                /p:GX_PROGRAM_DIR="${gxBasePath}" \
+                /p:localKbPath="${localKBPath}" \
+                /p:generatorName="${generatorName}" \
+                /p:propertyName="${propName}" \
+                /p:propertyValue="${propValue}" \
+                /t:SetGeneratorProperty
+            """
+    } catch (error) {
+        currentBuild.result = 'FAILURE'
+        throw error
+    }
+}
+
+return this
