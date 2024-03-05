830a223494f2d96a44430a1603fc2a0b2af74673
Julián Albarellos <53489408+JulianAlbarellos@users.noreply.github.com>
2024-03-05 09:41:08 -0300

    Fix method signature

--- src/com/genexus/PropertiesHelper.groovy
+++ src/com/genexus/PropertiesHelper.groovy
@@ -4,20 +4,20 @@ package com.genexus
  * This method set generator property using msbuild command
  * @param 
  */
-void setGeneratorProperty(String msbuildExePath, String gxBasePath, String localKBPath, String environmentName, String generatorName, String propName, String propValue) {
+void setGeneratorProperty(Map args = [:]) { // String msbuildExePath, String gxBasePath, String localKBPath, String environmentName, String generatorName, String propName, String propValue) {
     try {
         // Sync properties.msbuild -- TODO no sync if exists
         fileContents = libraryResource 'com/genexus/templates/properties.msbuild'
         writeFile file: 'properties.msbuild', text: fileContents
 
         bat script: """
-                "${msbuildExePath}" "${WORKSPACE}\\properties.msbuild" \
-                /p:GX_PROGRAM_DIR="${gxBasePath}" \
-                /p:localKbPath="${localKBPath}" \
-                /p:environmentName="${environmentName}" \
-                /p:generatorName="${generatorName}" \
-                /p:propertyName="${propName}" \
-                /p:propertyValue="${propValue}" \
+                "${args.msbuildExePath}" "${WORKSPACE}\\properties.msbuild" \
+                /p:GX_PROGRAM_DIR="${args.gxBasePath}" \
+                /p:localKbPath="${args.localKBPath}" \
+                /p:environmentName="${args.environmentName}" \
+                /p:generatorName="${args.generatorName}" \
+                /p:propertyName="${args.propName}" \
+                /p:propertyValue="${args.propValue}" \
                 /t:SetGeneratorProperty
             """
     } catch (error) {
