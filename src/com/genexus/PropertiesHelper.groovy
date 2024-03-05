de4de21f0ad6c60b8dd807064fbaab9b32b532d6
Julián Albarellos <53489408+JulianAlbarellos@users.noreply.github.com>
2024-03-05 09:02:48 -0300

    Add set generator property

--- src/com/genexus/PropertiesHelper.groovy
+++ src/com/genexus/PropertiesHelper.groovy
@@ -4,7 +4,7 @@ package com.genexus
  * This method set generator property using msbuild command
  * @param 
  */
-void setGeneratorProperty(String msbuildExePath, String gxBasePath, String localKBPath, String generatorName, String propName, String propValue) {
+void setGeneratorProperty(String msbuildExePath, String gxBasePath, String localKBPath, String environmentName, String generatorName, String propName, String propValue) {
     try {
         // Sync properties.msbuild -- TODO no sync if exists
         fileContents = libraryResource 'com/genexus/templates/properties.msbuild'
@@ -14,6 +14,7 @@ void setGeneratorProperty(String msbuildExePath, String gxBasePath, String local
                 "${msbuildExePath}" "${WORKSPACE}\\properties.msbuild" \
                 /p:GX_PROGRAM_DIR="${gxBasePath}" \
                 /p:localKbPath="${localKBPath}" \
+                /p:environmentName="${environmentName}" \
                 /p:generatorName="${generatorName}" \
                 /p:propertyName="${propName}" \
                 /p:propertyValue="${propValue}" \
