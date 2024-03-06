25d53102eb5262855f1dd8330079311c2441ccd8
Julián Albarellos <53489408+JulianAlbarellos@users.noreply.github.com>
2024-03-06 07:19:49 -0300

    Add helpers to kbproperties

--- src/com/genexus/PropertiesHelper.groovy
+++ src/com/genexus/PropertiesHelper.groovy
@@ -1,10 +1,40 @@
 package com.genexus
 
 /**
- * This method set generator property using msbuild command
+ * This method get envronment property using msbuild command
  * @param 
  */
-void setGeneratorProperty(Map args = [:], String generatorName, String propName, String propValue) {
+String getEnvironmentProperty(Map args = [:], String envPropName) {
+    try {
+                // Sync properties.msbuild -- TODO no sync if exists
+        fileContents = libraryResource 'com/genexus/templates/properties.msbuild'
+        writeFile file: 'properties.msbuild', text: fileContents
+
+        def propsFile = "${WORKSPACE}\\CommProperty.json"
+        bat script: """
+            "${args.msbuildExePath}" "${WORKSPACE}\\properties.msbuild" \
+            /p:GX_PROGRAM_DIR="${args.gxBasePath}" \
+            /p:localKbPath="${args.localKBPath}" \
+            /p:environmentName="${args.environmentName}" \
+            /p:environmentPropName="${envPropName}" \
+            /p:propFileAbsolutePath="${propsFile}" \
+            /p:helperName="aux" \
+            /t:GetEnvironmentProperty
+        """
+        commiteableGenPropValue = file.readJsonFile(propsFile)
+        echo "[READ] Generator property `${genPropName} = ${commiteableGenPropValue.aux}"
+        return commiteableGenPropValue.aux
+    } catch (error) {
+        currentBuild.result = 'FAILURE'
+        throw error
+    }
+}
+
+/**
+ * This method set environment property using msbuild command
+ * @param 
+ */
+void setEnvironmentProperty(Map args = [:], String envPropName, String envPropValue) {
     try {
         // Sync properties.msbuild -- TODO no sync if exists
         fileContents = libraryResource 'com/genexus/templates/properties.msbuild'
@@ -15,9 +45,65 @@ void setGeneratorProperty(Map args = [:], String generatorName, String propName,
                 /p:GX_PROGRAM_DIR="${args.gxBasePath}" \
                 /p:localKbPath="${args.localKBPath}" \
                 /p:environmentName="${args.environmentName}" \
-                /p:generatorName="${generatorName}" \
-                /p:propertyName="${propName}" \
-                /p:propertyValue="${propValue}" \
+                /p:environmentPropName="${environmentPropName}" \
+                /p:environmentPropValue="${environmentPropValue}" \
+                /t:SetEnvironmentProperty
+            """
+    } catch (error) {
+        currentBuild.result = 'FAILURE'
+        throw error
+    }
+}
+
+/**
+ * This method get generator property using msbuild command
+ * @param 
+ */
+String getGeneratorProperty(Map args = [:], String generatorName, String genPropName) {
+    try {
+        // Sync properties.msbuild -- TODO no sync if exists
+        fileContents = libraryResource 'com/genexus/templates/properties.msbuild'
+        writeFile file: 'properties.msbuild', text: fileContents
+
+        def propsFile = "${WORKSPACE}\\CommProperty.json"
+        bat script: """
+            "${args.msbuildExePath}" "${WORKSPACE}\\properties.msbuild" \
+            /p:GX_PROGRAM_DIR="${args.gxBasePath}" \
+            /p:localKbPath="${args.localKBPath}" \
+            /p:environmentName="${args.environmentName}" \
+            /p:generatorName="${genName}" \
+            /p:generatorPropName="${genPropName}" \
+            /p:propFileAbsolutePath="${propsFile}" \
+            /p:helperName="aux" \
+            /t:GetGeneratorProperty
+        """
+        commiteableGenPropValue = file.readJsonFile(propsFile)
+        echo "[READ] Generator property `${genPropName} = ${commiteableGenPropValue.aux}"
+        return commiteableGenPropValue.aux
+    } catch (error) {
+        currentBuild.result = 'FAILURE'
+        throw error
+    }
+}
+
+/**
+ * This method set generator property using msbuild command
+ * @param 
+ */
+void setGeneratorProperty(Map args = [:], String genName, String genPropName, String genPropValue) {
+    try {
+        // Sync properties.msbuild -- TODO no sync if exists
+        fileContents = libraryResource 'com/genexus/templates/properties.msbuild'
+        writeFile file: 'properties.msbuild', text: fileContents
+
+        bat script: """
+                "${args.msbuildExePath}" "${WORKSPACE}\\properties.msbuild" \
+                /p:GX_PROGRAM_DIR="${args.gxBasePath}" \
+                /p:localKbPath="${args.localKBPath}" \
+                /p:environmentName="${args.environmentName}" \
+                /p:generatorName="${genName}" \
+                /p:generatorPropName="${genPropName}" \
+                /p:generatorPropValue="${genPropValue}" \
                 /t:SetGeneratorProperty
             """
     } catch (error) {
