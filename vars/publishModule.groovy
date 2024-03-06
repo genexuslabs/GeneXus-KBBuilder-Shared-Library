b1a9620fb293ce355fbf6fdaae418da7e28be533
Julián Albarellos <53489408+JulianAlbarellos@users.noreply.github.com>
2024-03-06 14:17:35 -0300

    Fix package to use semver

--- vars/publishModule.groovy
+++ vars/publishModule.groovy
@@ -1,32 +1,36 @@
 /*
- * Job publishModule >> 
- * -- >> Task documentation:: https://wiki.genexus.com/commwiki/wiki?55011,Modules%20MsBuild%20Tasks%20%28GeneXus%2018%20Upgrade%203%20or%20prior%29#PublishModule+Task
+ * Job publishModule >> This job publishes a GeneXus module using the PublishGXModule MSBuild task. 
+ * -- >> For detailed information on the task refer to the documentation https://wiki.genexus.com/commwiki/wiki?55011,Modules%20MsBuild%20Tasks%20%28GeneXus%2018%20Upgrade%203%20or%20prior%29#PublishModule+Task
  *
- * @Param args = [:]
- * +- gxBasePath
- * +- localKBPath
- * +- environmentName
- * +- propertiesFilePath
- * +- machineFilePath
+ * Parameters:
+ * - args: A map containing the following parameters:
+ *   - gxBasePath: The path of the GeneXus installation.
+ *   - localKBPath: The local path of the Knowledge Base.
+ *   - moduleServerCredentialsId: The credentials ID containing the username and password for the module server.
+ *   - localModulePackage: The local path to the module package.
+ *   - moduleServerId: The ID of the module server.
+ *
  */
 
 def call(Map args = [:]) {
     // Sync properties.msbuild
     def fileContents = libraryResource 'com/genexus/templates/cdxci.msbuild'
     writeFile file: 'cdxci.msbuild', text: fileContents
-    
-    def moduleTargetPath = "${args.localKBPath}\\IntegrationModule\\${args.packageModuleName}\\${args.buildJobNumber}"
 
-    bat script: """
+    withCredentials([ usernamePassword( credentialsId: args.moduleServerCredentialsId,
+        usernameVariable: 'username',
+        passwordVariable: 'password')
+    ]) {
+        bat label: "Package Module::${args.packageModuleName}",
+        script: """
             "${args.msbuildExePath}" "${WORKSPACE}\\cdxci.msbuild" \
             /p:GX_PROGRAM_DIR="${args.gxBasePath}" \
             /p:localKbPath="${args.localKBPath}" \
-            /p:packageModuleName="${args.packageModuleName}" \
-            /p:pipelineBuildNumber="${args.buildJobNumber}" \
-            /p:csharpEnvName="${args.csharpEnvName}" \
-            /p:javaEnvName="${args.javaEnvName}" \
-            /p:netCoreEnvName="${args.netCoreEnvName}" \
-            /p:destinationPath="${moduleTargetPath}" \
-            /t:PackageGXModule
+            /p:opcPath="${args.localModulePackage}" \
+            /p:serverId="${args.moduleServerId}" \
+            /p:serverUsername="${username}" \
+            /p:serverPassword="${password}" \
+            /t:PublishGXModule
         """
+    }
 }
