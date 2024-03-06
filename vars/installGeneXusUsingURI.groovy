5d2dd9d2699dcfec7b1d04b633275ab2f7636a50
Julián Albarellos <53489408+JulianAlbarellos@users.noreply.github.com>
2024-03-06 10:49:49 -0300

    Improve description

--- vars/installGeneXusUsingURI.groovy
+++ vars/installGeneXusUsingURI.groovy
@@ -1,22 +1,32 @@
 /*
- * Job installGeneXusUsingURI:
- * -- >> IF [forceUpdateGX] >> DELETE GX INSTALLATION
- * -- >> COMPARE [genexusURI] WITH [gxBasePath]\\LastURI.txt
- * -- -- >> UPDATE [gxBasePath] FROM [genexusURI]
- * -- -- >> CONFIG ProgramData and UserData DIRECTORIES
- * -- -- >> RUN [gxBasePath]\\install
- * -- -- >> IF [localAndroidSDKPath] DOWNLOAD USING [gxBasePath]\Android\Setup\AndroidRequirements.exe
- * -- >> CONFIGURE [protServerType], [protServerName], [protServerCredentialsId]
- * -- >> PRINT GENEXUS VERSION
+ * Job: installGeneXusUsingURI
  *
- * @Param args = [:]
- * +- forceUpdateGX
- * +- gxBasePath
- * +- localAndroidSDKPath
- * +- genexusURI
- * +- protServerType, protServerName, protServerCredentialsId
+ * Description:
+ * This job performs a series of tasks to manage and update a GeneXus installation. It includes options
+ * to force update the GeneXus installation, compare and update the GeneXus URI, configure ProgramData
+ * and UserData directories, run the GeneXus installation, download Android SDK if necessary, configure
+ * Protection Server settings, and print the GeneXus version.
+ *
+ * Parameters:
+ * - args: A map containing the following parameters:
+ *   - forceUpdateGX: Boolean flag indicating whether to force the update of the GeneXus installation.
+ *   - gxBasePath: The base path of the GeneXus installation.
+ *   - localAndroidSDKPath: The local path of the Android SDK.
+ *   - genexusURI: The GeneXus URI for updating the installation.
+ *   - protServerType: Type of Protection Server.
+ *   - protServerName: Name or address of the Protection Server.
+ *   - protServerCredentialsId: Credentials ID containing username and password for the Protection Server.
+ *
+ * Workflow Steps:
+ * 1. If ${forceUpdateGX} is true, delete the existing GeneXus installation.
+ * 2. Compare genexusURI with ${gxBasePath}\LastURI.txt and update the installation accordingly.
+ * 3. Configure ProgramData and UserData directories.
+ * 4. Execute ${gxBasePath}\genexus.com /install
+ * 5. If ${localAndroidSDKPath} is provided, download Android SDK using ${gxBasePath}\AndroidRequirements.exe.
+ * 6. Configure Protection Server.
+ * 7. Print the GeneXus installation version.
+ *
  */
-
 import com.genexus.GeneXusHelper
 
 def call(Map args = [:]) {
