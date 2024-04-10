cc62cceddae83c968764941eb74e4e5e6d60f354
Julián Albarellos <53489408+JulianAlbarellos@users.noreply.github.com>
2024-04-10 13:55:11 -0300

    Fix 5

--- src/com/genexus/GXDeployEngineHelper.groovy
+++ src/com/genexus/GXDeployEngineHelper.groovy
@@ -14,70 +14,14 @@ void createDockerContext(Map args = [:]) {
                 /p:DOCKER_MAINTAINER="GeneXus DevOps Team <devops@genexus.com>" \
                 /p:DOCKER_IMAGE_NAME="${args.dockerImageName.toLowerCase()}" \
                 /p:DOCKER_BASE_IMAGE="${args.dockerBaseImage}" \
-                /p:DeployFullPath="${args.localKBPath}\\${args.targetPath}\\${args.duName}\\${args.duName}\\${env.BUILD_NUMBER}" \
                 /p:DeploySource="${args.packageLocation}" \
                 /p:CreatePackageScript="createpackage.msbuild" \
                 /p:WebSourcePath="${args.localKBPath}\\${args.targetPath}\\web" \
                 /p:ProjectName="${args.duName}_${env.BUILD_NUMBER}" \
-                /p:DOCKER_WEBAPPLOCATION="${args.duName}" \
+                /p:DOCKER_WEBAPPLOCATION="${args.webAppLocation}" \
                 /t:CreatePackage
             """
-            /*
-        String target = "\"${args.gxBasePath}\\CreateCloudPackage.msbuild\" /t:CreatePackage"
-        String msbuildGenArgs = ''
-        msbuildGenArgs = concatArgs(msbuildGenArgs, "GX_PROGRAM_DIR", "${localGXPath}")
-        def duExtentionName = ''
-        String projectName = "${deploymentUnitDefinition.name}_${env.BUILD_NUMBER}"
-        switch (generatorLenguage) {
-            case 'Java':
-                def auxFileExtention = powershell label: "Find generated Java file",
-                    script: """
-                        \$deployPath = \"${integrationDeployTargetPath}\\\\${deploymentUnitDefinition.name}\"
-                        \$dirs = Get-ChildItem -Path \$deployPath 
-                        foreach(\$i in \$dirs) {
-                            \$file = Get-ChildItem -Path \$deployPath\\\$i | Where-Object { \$_.Name.StartsWith(\"${projectName}\" + \".\")}
-                            \$aux = \$file.name
-                            if(![string]::IsNullOrEmpty(\$aux)) {
-                                \$aux.Replace(\"${projectName}\", \"\")
-                            } 
-                        }
-                    """, returnStdout: true
-                def fileExtention = auxFileExtention.trim()
-                echo " DEBUG fileExtention::${fileExtention}"
-                if(fileExtention.equals(".jar")) {
-                    if(deploymentUnitDefinition.urlSubdirectory) {
-                        msbuildGenArgs = concatArgs(msbuildGenArgs, "JarName", deploymentUnitDefinition.urlSubdirectory)
-                    }
-                    else {
-                        msbuildGenArgs = concatArgs(msbuildGenArgs, "JarName", "ROOT")
-                    }
-                }
-                else {
-                    if(deploymentUnitDefinition.urlSubdirectory) {
-                        msbuildGenArgs = concatArgs(msbuildGenArgs, "WarName", deploymentUnitDefinition.urlSubdirectory)
-                    }
-                    else {
-                        msbuildGenArgs = concatArgs(msbuildGenArgs, "WarName", "ROOT")
-                    }
-                }
-                duExtentionName = "${projectName}${fileExtention}"
-                break
-
-            case '.NET':
-                duExtentionName = "${projectName}.zip"
-                break
-
-            default:
-                currentBuild.result = 'FAILURE'
-                error 'Generator is not Java or .Net.'
-                break
-        }
-
-        bat label: 'Creating Docker context', 
-            script: "\"${msbuildExePath}\" ${target} ${msbuildGenArgs} /nologo "
-
-        powershell script: "Remove-Item -Path \"${integrationDeployTargetPath}\\${deploymentUnitDefinition.name}\\${duExtentionName}\" -Recurse"
-        */
+//                /p:DeployFullPath="${args.localKBPath}\\${args.targetPath}\\${args.duName}\\${args.duName}\\${env.BUILD_NUMBER}" \
     } catch (error) {
         currentBuild.result = 'FAILURE'
         echo " ERROR ${error.getMessage()}"
