22ca773496b075308953a3e5478b5c50694c2d17
Julián Albarellos <53489408+JulianAlbarellos@users.noreply.github.com>
2024-07-30 17:18:34 -0300

    Add AWS helper module

---
+++ src/com/genexus/CloudHelper.groovy
@@ -0,0 +1,31 @@
+package com.genexus
+
+/**
+ * Configure Windows AWS CLI
+ *
+ * @param args A map containing the following parameters:
+ *   - awsRegion: Amazon region (e.g., us-east-1)
+ *   - awsOutput: Amazon output format (e.g., json)
+ *   - awsCredentialsId: AWS AccessKey and SecretAccessKey stored as a Jenkins credential of type username/password
+ */
+void awsConfigure(Map args = [:]) {
+    try{
+        echo "Configure ${args.awsCredentialsId}"
+        withCredentials([
+            usernamePassword(
+                credentialsId: args.awsCredentialsId,
+                passwordVariable: 'awsPWD',
+                usernameVariable: 'awsUSER')
+        ]) {
+            powershell script: "aws configure set aws_access_key_id ${awsUSER} --profile ${args.awsCredentialsId} "
+            powershell script: "aws configure set aws_secret_access_key \$ENV:awsPWD --profile ${args.awsCredentialsId} " 
+            powershell script: "aws configure set region ${args.awsRegion} --profile ${args.awsCredentialsId} "
+            powershell script: "aws configure set output ${args.awsOutput} --profile ${args.awsCredentialsId}"
+        }
+    } catch (error) {
+        currentBuild.result = 'FAILURE'
+        throw error
+    }
+}
+
+return this
