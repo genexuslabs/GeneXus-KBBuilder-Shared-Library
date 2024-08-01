e9d40470b8314b326d595e45e5e545ab2b6aa136
Julián Albarellos <53489408+JulianAlbarellos@users.noreply.github.com>
2024-08-01 11:47:20 -0300

    Add notifications

--- src/com/genexus/NotificationHelper.groovy
+++ src/com/genexus/NotificationHelper.groovy
@@ -1,6 +1,5 @@
 package com.genexus
 import groovy.text.StreamingTemplateEngine
-import com.genexus.GeneXusHelper
 
 /**
  * This method
@@ -51,70 +50,125 @@ String getChangeLogSet() {
     return revisions
 }
 
-Map getBuildInfo() {
-    Map ret = [:]
-    switch (currentBuild.currentResult) {
-        case 'SUCCESS':
-            ret.icon = "✅"
-            ret.buildColor = "green"
-            ret.buildResult = "Success"
-        break
-        case 'UNSTABLE':
-            ret.icon = "⚠️"
-            ret.buildColor = "#FFC300"
-            ret.buildResult = "Unstable"
-        break
-        case 'FAILURE':
-            ret.icon = "❌"
-            ret.buildColor = "red"
-            ret.buildResult = "Failure"
-        break
-        case 'NOT_BUILT':
-            ret.icon = "🔳"
-            ret.buildColor = "black"
-            ret.buildResult = "Not build"
-        break
-        case 'ABORTED':
-            ret.icon = "⛔"
-            ret.buildColor = "orange"
-            ret.buildResult = "Aborted"
-        break
-        default:
-            ret.icon = "🚨"
-            ret.buildColor = "red"
-            ret.buildResult = "Unknown"
-        break
-    }
-    return ret
-}
-
-
-void sendEmail(Map args = [:]) {
-    String flag = "[${args.notificationWhen}]"
-    if(flag.contains(currentBuild.currentResult)) {
-        def gxHelper = new GeneXusHelper()
-        String gxVersion = gxHelper.getGeneXusInstallationVersion(args.gxBasePath)
-        def changeLogSet = getChangeLogSet()
-        Map emailConst = getBuildInfo()
-
-        String templateName = "com/genexus/notificationTemplates/emailBuildResult.html.groovy"
-        def template = createTemplate(templateName, [
-            "jenkinsUrl"        :   env.BUILD_URL,
-            "jenkinsTimestamp"  :   env.BUILD_TIMESTAMP,
-            "buildNumber"       :   env.BUILD_NUMBER,
-            "buildColor"        :   emailConst.buildColor,
-            "buildResult"       :   emailConst.buildResult,
-            "jenkinsDuration"   :   currentBuild.durationString.replaceAll(' and counting', ''),
-            "changeLogSet"      :   changeLogSet,
-            "cause"             :   currentBuild.buildCauses[0].shortDescription.replaceAll('\\[',' '),
-            "gxversion"         :   gxVersion
-        ]);
-
-        emailext body: template,
-            mimeType: 'text/html',
-            subject: "${emailConst.icon} ${currentBuild.fullDisplayName}",
-            to: "${args.notificationRecipient}, cc:${args.notificationBaseList.replace(', ', ', cc:')}",
-            attachLog: true
-    }
-}
 return this
+
+// package com.genexus
+// import groovy.text.StreamingTemplateEngine
+// import com.genexus.GeneXusHelper
+
+// /**
+//  * This method
+//  * @param 
+//  */
+// String createTemplate(String templateName, def params) {
+//     def fileContents = libraryResource(templateName)
+//     def engine = new StreamingTemplateEngine()
+//     return engine.createTemplate(fileContents).make(params).toString()
+// }
+
+// /**
+//  * This methods 
+//  * @param 
+//  */
+// @NonCPS 
+// String getChangeLogSet() {
+//     String revisions = ""
+//     def changeLogSets = currentBuild.changeSets
+//     if(changeLogSets.size() != 0) {
+//         Boolean isEven = false
+//         for (def entry in changeLogSets) {
+//             for (def revision in entry.items) {
+//                 def date = new Date(revision.timestamp)
+//                 if(isEven) { revisions += "<tr class=\"revisions-even\">" }
+//                 else { revisions += "<tr class=\"revisions-odd\">" }
+//                 isEven = !(isEven)
+//                 revisions +="<th class=\"revision-item\">${revision.commitId.toString()}</th>"
+//                 revisions +="<th class=\"revision-item\" style=\"width:240px;\">${date.toString()}</th>"
+//                 revisions +="<th class=\"revision-item\" style=\"text-align:left;padding-left:5px;\">${revision.author.toString()}</th>"
+//                 revisions +="<th class=\"revision-item\" style=\"text-align:left;padding-left:5px;\">${revision.msg.toString()}</th>"
+//                 def count = 0
+//                 def files = new ArrayList(revision.affectedFiles)
+//                 for (def file in files) {
+//                 //    echo " ${file.editType.name} ${file.path}"
+//                     count += 1
+//                 }
+//                 revisions +="<th class=\"revision-item\" style=\"text-align:center;padding-left:5px;width:60px;\">${count.toString()} Files</th>"
+//                 revisions += "</tr>"
+//             }
+//         }
+//     }
+//     else {
+//         revisions += "<tr class=\"revisions-even\">"
+//         revisions +="<th class=\"revision-item\">No changes</th>"
+//         revisions += "</tr>"
+//     }
+//     return revisions
+// }
+
+// Map getBuildInfo() {
+//     Map ret = [:]
+//     switch (currentBuild.currentResult) {
+//         case 'SUCCESS':
+//             ret.icon = "✅"
+//             ret.buildColor = "green"
+//             ret.buildResult = "Success"
+//         break
+//         case 'UNSTABLE':
+//             ret.icon = "⚠️"
+//             ret.buildColor = "#FFC300"
+//             ret.buildResult = "Unstable"
+//         break
+//         case 'FAILURE':
+//             ret.icon = "❌"
+//             ret.buildColor = "red"
+//             ret.buildResult = "Failure"
+//         break
+//         case 'NOT_BUILT':
+//             ret.icon = "🔳"
+//             ret.buildColor = "black"
+//             ret.buildResult = "Not build"
+//         break
+//         case 'ABORTED':
+//             ret.icon = "⛔"
+//             ret.buildColor = "orange"
+//             ret.buildResult = "Aborted"
+//         break
+//         default:
+//             ret.icon = "🚨"
+//             ret.buildColor = "red"
+//             ret.buildResult = "Unknown"
+//         break
+//     }
+//     return ret
+// }
+
+
+// void sendEmail(Map args = [:]) {
+//     String flag = "[${args.notificationWhen}]"
+//     if(flag.contains(currentBuild.currentResult)) {
+//         def gxHelper = new GeneXusHelper()
+//         String gxVersion = gxHelper.getGeneXusInstallationVersion(args.gxBasePath)
+//         def changeLogSet = getChangeLogSet()
+//         Map emailConst = getBuildInfo()
+
+//         String templateName = "com/genexus/notificationTemplates/emailBuildResult.html.groovy"
+//         def template = createTemplate(templateName, [
+//             "jenkinsUrl"        :   env.BUILD_URL,
+//             "jenkinsTimestamp"  :   env.BUILD_TIMESTAMP,
+//             "buildNumber"       :   env.BUILD_NUMBER,
+//             "buildColor"        :   emailConst.buildColor,
+//             "buildResult"       :   emailConst.buildResult,
+//             "jenkinsDuration"   :   currentBuild.durationString.replaceAll(' and counting', ''),
+//             "changeLogSet"      :   changeLogSet,
+//             "cause"             :   currentBuild.buildCauses[0].shortDescription.replaceAll('\\[',' '),
+//             "gxversion"         :   gxVersion
+//         ]);
+
+//         emailext body: template,
+//             mimeType: 'text/html',
+//             subject: "${emailConst.icon} ${currentBuild.fullDisplayName}",
+//             to: "${args.notificationRecipient}, cc:${args.notificationBaseList.replace(', ', ', cc:')}",
+//             attachLog: true
+//     }
+// }
+// return this
---
+++ vars/sendEmailNotification.groovy
@@ -0,0 +1,97 @@
+import groovy.text.StreamingTemplateEngine
+import com.genexus.NotificationHelper
+
+/**
+ * This method
+ * @param 
+ */
+String createTemplate(String templateName, def params) {
+    def fileContents = libraryResource(templateName)
+    def engine = new StreamingTemplateEngine()
+    return engine.createTemplate(fileContents).make(params).toString()
+}
+
+
+/*
+ * Job readPipelineProperties >> Read properties from build.properties
+ *
+ * @Param args = [:]
+ * +- gitUrl
+ * +- gitBranch
+ * +- gitCredentialsId
+ * +- propertiesFilePath
+ * +- machineFilePath
+ */
+
+def call(Map args = [:]) {
+    try {
+        def engine2 = new NotificationHelper()
+        def changeLogSet = engine2.getChangeLogSet()
+        def icon
+        String template
+        String templateName
+        String buildColor
+        String buildResult
+        String jobName = (env.JOB_NAME).replace(env.JOB_BASE_NAME, '')
+        String jobFullDisplayName = currentBuild.fullDisplayName
+        def splitJobDisplayName = currentBuild.fullDisplayName.split(' » ')
+        def jobDisplayName = "${jobFullDisplayName.replace(splitJobDisplayName[splitJobDisplayName.length - 1], '')}"
+        switch (currentBuild.currentResult) {
+            case 'SUCCESS':
+                icon = "✅"
+                buildColor = "green"
+                buildResult = "Success"
+            break
+            case 'UNSTABLE':
+                icon = "⚠️"
+                buildColor = "#FFC300"
+                buildResult = "Unstable"
+            break
+            case 'FAILURE':
+                icon = "❌"
+                buildColor = "red"
+                buildResult = "Failure"
+            break
+            case 'NOT_BUILT':
+                icon = "🔳"
+                buildColor = "black"
+                buildResult = "Not build"
+            break
+            case 'ABORTED':
+                icon = "⛔"
+                buildColor = "orange"
+                buildResult = "Aborted"
+            break
+            default:
+                icon = "🚨"
+                buildColor = "red"
+                buildResult = "Unknown"
+            break
+        }
+
+        templateName = "com/genexus/notificationTemplates/emailBuildResult.html.groovy"
+        template = createTemplate(templateName, [
+            "jenkinsJobName"    :   jobName,
+            "jenkinsUrl"        :   env.BUILD_URL,
+            "jenkinsTimestamp"  :   env.BUILD_TIMESTAMP,
+            "buildNumber"       :   env.BUILD_NUMBER,
+            "buildColor"        :   buildColor,
+            "buildResult"       :   buildResult,
+            "jenkinsDuration"   :   currentBuild.durationString.replaceAll(' and counting', ''),
+            "changeLogSet"      :   changeLogSet,
+            "cause"             :   currentBuild.buildCauses[0].shortDescription.replaceAll('\\[',' '),
+            "gxversion"         :   "18.0.0.0"//gxVersion
+        ]);
+
+        emailext body: template,
+            mimeType: 'text/html',
+            subject: "${icon} ${jobDisplayName.toString()} Build #${env.BUILD_NUMBER} » ${currentBuild.currentResult}",
+            to: "jalbarellos@genexus.com",
+            replyTo: "jalbarellos@genexus.com",
+            attachLog: true
+
+    } catch (def error) {
+        currentBuild.result = 'FAILURE'
+        throw error
+    }
+}
