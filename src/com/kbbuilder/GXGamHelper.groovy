3ebcaf7633cd8dd6566bc34b478318cf326eb8b8
Julián Albarellos <53489408+JulianAlbarellos@users.noreply.github.com>
2024-11-09 11:14:39 -0300

    Add update gam init resources

--- src/com/kbbuilder/GXGamHelper.groovy
+++ src/com/kbbuilder/GXGamHelper.groovy
@@ -93,6 +93,66 @@ String updatePlatform(Map args = [:]) {
     }
 }
 
+String updateInitResources(Map args = [:]) {
+    try{
+        def gxLibDeployEngine = new GXDeployEngineHelper()
+        // ----------------------------- Print Debug vars
+        echo "INFO DeployTarget:: ${args.deployTarget}"
+        echo "INFO PackagerResourcesDirPath:: ${args.packagerResourcesDirPath}"
+        echo "INFO PackageAPIFile:: ${args.packageAPIFile}"
+        echo "INFO PackageTarget:: ${args.packageTarget}"
+        echo "INFO GamAPIResourcesRepository:: ${args.gamAPIResourcesRepository}"
+        echo "INFO ExtObjGeneratorName:: ${args.extObjGeneratorName}"
+        // ----------------------------- Clean daployTarget
+        powershell script: """
+            \$ErrorActionPreference = 'Stop'
+            if (Test-Path -Path "${envJavaSQLDefinition.deployTarget}") { Remove-Item -Path "${envJavaSQLDefinition.deployTarget}" -Recurse -Force }
+            \$null = New-Item -Path "${envJavaSQLDefinition.deployTarget}" -ItemType Directory
+        """
+        // ----------------------------- Create GAM Init Resources package
+        bat script: """
+            "${args.msbuildExePath}" "${args.localKBPath}\\${args.targetPath}\\Web\\${args.packageAPIFile}" \
+            /p:GX_PROGRAM_DIR="${args.gxBasePath}" \
+            /p:KBGAMDirectory="${args.localKBPath}" \
+            /p:KBEnvironment="${args.environmentName}" \
+            /p:ModelDir="${args.targetPath}" \
+            /p:Generator="${args.generatedLanguage}" \
+            /p:DBMS="${args.dataSource}" \
+            /p:GenerateLibraryPath="${args.deployTarget}" \
+            /p:PackagerResources="${args.packagerResourcesDirPath}" \
+            /t:${args.packageTarget}
+        """
+        // ----------------------------- Rename package for nuget standar
+        args.packageName = powershell script: """
+            \$ErrorActionPreference = 'Stop'
+            \$packageFileName = (Get-ChildItem -Path "${args.deployTarget}" -Filter '*.zip').Name
+            write-host "Package Name:: \$packageFileName"
+            \$packageFullPath = Join-Path "${args.deployTarget}" "\$packageFileName"
+            write-host "Package Full Path:: \$packageFullPath"
+            Rename-Item -Path \$packageFullPath -NewName \$packageFileName.replace('GAM', '') -Force
+            (Get-ChildItem -Path "${args.deployTarget}" -Filter '*.zip').Name
+        """, returnStdout: true
+        echo "[INFO] Package Name:: ${args.packageName.trim()}"
+        args.packageLocation = "${args.deployTarget}\\${args.packageName.trim()}"
+        echo "[INFO] Package Location:: ${args.packageLocation}"
+        // ----------------------------- Archive artifacts
+        dir("${args.deployTarget}") {
+            archiveArtifacts artifacts: "${args.packageName.trim()}", followSymlinks: false
+        }
+        // ----------------------------- Create NuGet package
+        args.packageName = args.packageName.replace(".zip", "").trim()
+        args.packageVersion = args.componentVersion
+        args.nupkgPath = gxLibDeployEngine.createNuGetPackageFromZip(args)
+
+        // ----------------------------- Publish NuGet package
+        args.moduleServerSource = "${args.moduleServerSourceBase}${args.artifactsServerId}"
+        gxLibDeployEngine.publishNuGetPackage(args)
+    } catch (error) {
+        currentBuild.result = 'FAILURE'
+        throw error
+    }
+}
+
 void publishPlatform(Map args = [:]) {
     try{
 
