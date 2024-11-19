a12ce300a22996cf99911c11014c8d87cc99143b
Julián Albarellos <53489408+JulianAlbarellos@users.noreply.github.com>
2024-11-19 12:48:48 -0300

    Add updatePlatform

--- src/com/kbbuilder/GXFlowHelper.groovy
+++ src/com/kbbuilder/GXFlowHelper.groovy
@@ -1,45 +1,126 @@
-package com.genexus
+package com.kbbuilder
+import com.genexus.PropertiesHelper
 
 /**
- * Configures module server settings for the GeneXus environment.
- * -- >> For detailed information on the task, refer to the documentation: https://wiki.genexus.com/commwiki/wiki?46830,Modules+MSBuild+Tasks#AddModulesServer+Task
- *
- * @param args A map containing the following parameters:
- *   - gxBasePath: The base path of the GeneXus installation.
- *   - moduleServerId: Name or URL of the module server.
- *   - moduleServerSource: module server source URL.
- *   - moduleServerCredentialsId: Jenkins credentials ID for module server authentication.
+ * Return packageName for packageList in dispatch
+ */
+String updatePlatformNetFW(Map envArgs = [:], Map clientDuArgs = [:], Map engineDuArgs = [:]) {
+    try{
+        def kbLibHelper = new PropertiesHelper()
+        // ----------------------------- Print Debug vars
+        echo "INFO generatedLanguage:: ${envArgs.generatedLanguage}"
+        echo "INFO dataSource:: ${envArgs.dataSource}"
+        envArgs.targetPath = "${envArgs.generatedLanguage}${envArgs.dataSource}"
+
+        stage("Prepare ENV:${envArgs.targetPath}") {
+            steps {
+                script {
+                    kbLibHelper.setEnvironmentProperty(envArgs, "TargetPath", envArgs.targetPath)
+
+                    powershell script: """
+                        \$ErrorActionPreference = 'Stop'
+                        # --------------------- Remove Model Dir
+                        if (Test-Path -Path "${envArgs.localKBPath}\\${envArgs.targetPath}") { Remove-Item -Path "${envArgs.localKBPath}\\${envArgs.targetPath}" -Recurse -Force }
+                        \$null = New-Item -Path "${envArgs.localKBPath}\\${envArgs.targetPath}\\web\\bin" -ItemType Directory
+
+                        # --------------------- TODO: Update reference to circle dependency
+                        Write-Output((Get-Date -Format G) + " [INFO] Sync ${WORKSPACE}\\Libs\\Net\\*")
+                        Copy-Item -Path "${WORKSPACE}\\Libs\\Net\\*" -Destination "${envArgs.localKBPath}\\${envArgs.targetPath}\\web\\bin\\" -Force -Recurse
+                        
+                        # --------------------- Sync files from GeneXus Installation
+                        Write-Output((Get-Date -Format G) + " [INFO] Sync ${envArgs.gxBasePath}\\Packages\\Gxpm\\RuleEvaluator\\dotNet\\GXflow.Programs.Rules.dll")
+                        copy-item -Path "${envArgs.gxBasePath}\\Packages\\Gxpm\\RuleEvaluator\\dotNet\\GXflow.Programs.Rules.dll" -Destination "${envArgs.localKBPath}\\${envArgs.targetPath}\\web\\bin\\GXflow.Programs.Rules.dll" -Force
+                        Write-Output((Get-Date -Format G) + " [INFO] Sync ${envArgs.gxBasePath}\\Packages\\Gxpm\\WFCache\\dotNet\\wfcache.dll")
+                        copy-item -Path "${envArgs.gxBasePath}\\Packages\\Gxpm\\WFCache\\dotNet\\wfcache.dll" -Destination "${envArgs.localKBPath}\\${envArgs.targetPath}\\web\\bin\\wfcache.dll" -Force
+                        Write-Output((Get-Date -Format G) + " [INFO] Extract NetProt.zip")
+                        Expand-Archive -Path "${envArgs.gxBasePath}\\Packages\\GXPM\\Protection\\NetProt.zip" -DestinationPath "${envArgs.localKBPath}\\${envArgs.targetPath}\\web\\bin" -Force                        
+                        
+                        # --------------------- Create VirtualDirCreationDisabled file
+                        Write-Output((Get-Date -Format G) + " [INFO] Create VirtualDirCreationDisabled file")
+                        if(-not (Test-Path -Path "${envArgs.localKBPath}\\${envArgs.targetPath}\\VirtualDirCreationDisabled")) {\$null = New-Item -Path "${envArgs.localKBPath}\\${envArgs.targetPath}\\VirtualDirCreationDisabled"}
+                    """
+                }
+            }
+        }
+        stage("Build ENV:${envArgs.targetPath}") {
+            steps{
+                script {
+                    kbLibHelper.setEnvironmentProperty(envArgs, "translation_type", "Run-time")
+                    kbLibHelper.setEnvironmentProperty(envArgs, "html_document_type", "HTML5")
+                    kbLibHelper.setGeneratorProperty(envArgs, "Default", "isolation_level", "Read Uncommitted")
+                    kbLibHelper.setGeneratorProperty(envArgs, "Default", "reorganization_options", "-nogui -noverifydatabaseschema")
+                    kbLibHelper.setGeneratorProperty(envArgs, "Default", ".Net Application Namespace", "QueryViewer.Services")                    
+                    
+                    buildConfigurationEnvironment(envArgs)
+
+                    // //------------------ W.A for connection.gam
+                    bat label: 'Create connection.gam',
+                        script: "echo > \"${envArgs.localKBPath}\\${envArgs.targetPath}\\web\\connection.gam\""
+                }
+            }
+        }
+        stage("Package ENV:${envArgs.targetPath} Platform"){
+            steps {
+                script {
+
+                    clientDuArgs.packageLocation = packageLocalDU(clientDuArgs)
+                    echo "INFO DU Package Location:: ${clientDuArgs.packageLocation}"
+                    deployDirPath = powershell script: """Split-Path "${clientDuArgs.packageLocation}" -Parent""", returnStdout: true
+                    echo "INFO Deploy Dir Path:: ${deployDirPath}"
+                    packageName = powershell script: """Split-Path "${clientDuArgs.packageLocation}" -Leaf""", returnStdout: true
+                    echo "INFO Package Name:: ${packageName}"
+                    // dir("${deployDirPath.trim()}") {
+                    //     TODO: Rename file to NetFW_client/Engine_BUILDNUM.zip
+                    //     archiveArtifacts artifacts: "${packageName.trim()}", followSymlinks: false
+                    // }
+                    engineDuArgs.packageLocation = packageLocalDU(engineDuArgs)
+                    echo "INFO DU Package Location:: ${engineDuArgs.packageLocation}"
+                    deployDirPath = powershell script: """Split-Path "${engineDuArgs.packageLocation}" -Parent""", returnStdout: true
+                    echo "INFO Deploy Dir Path:: ${deployDirPath}"
+                    packageName = powershell script: """Split-Path "${engineDuArgs.packageLocation}" -Leaf""", returnStdout: true
+                    echo "INFO Package Name:: ${packageName}"
+                    // dir("${deployDirPath.trim()}") {
+                    //     TODO: Rename file to NetFW_client/Engine_BUILDNUM.zip
+                    //     archiveArtifacts artifacts: "${packageName.trim()}", followSymlinks: false
+                    // }
+                    
+                }
+            }
+        }
+    } catch (error) {
+        currentBuild.result = 'FAILURE'
+        throw error
+    }
+}
+
+/**
  *
  */
-void configureModuleServer(Map args = [:]) {
+void updatePlatformJava(Map envArgs = [:], Map clientDuArgs = [:], Map engineDuArgs = [:]) {
     try{
-        def fileContents = libraryResource 'com/genexus/templates/cdxci.msbuild'
-        writeFile file: 'cdxci.msbuild', text: fileContents
-        if(args.moduleServerCredentialsId) {
-            withCredentials([
-                usernamePassword(credentialsId: "${args.moduleServerCredentialsId}", passwordVariable: 'moduleServerPass', usernameVariable: 'moduleServerUser')
-            ]) {        
-                bat script: """
-                    "${args.msbuildExePath}" "${WORKSPACE}\\cdxci.msbuild" \
-                    /p:GX_PROGRAM_DIR="${args.gxBasePath}" \
-                    /p:ServerId="${args.moduleServerId}" \
-                    /p:ServerSource="${args.moduleServerSource}" \
-                    /p:ServerType="${args.moduleServerType}" \
-                    /p:ServerUsername="${moduleServerUser}" \
-                    /p:ServerPassword="${moduleServerPass}" \
-                    /t:AddModuleServer
-                """
-            }
-        } else {
-            bat script: """
-                "${args.msbuildExePath}" "${WORKSPACE}\\cdxci.msbuild" \
-                /p:GX_PROGRAM_DIR="${args.gxBasePath}" \
-                /p:ServerId="${args.moduleServerId}" \
-                /p:ServerSource="${args.moduleServerSource}" \
-                /p:ServerType="${args.moduleServerType}" \
-                /t:AddModuleServer
-            """
-        }
+        def kbLibHelper = new PropertiesHelper()
+        // ----------------------------- Print Debug vars
+        echo "INFO generatedLanguage:: ${envArgs.generatedLanguage}"
+        echo "INFO dataSource:: ${envArgs.dataSource}"
+        envArgs.targetPath = "${envArgs.generatedLanguage}${envArgs.dataSource}"
+
+    } catch (error) {
+        currentBuild.result = 'FAILURE'
+        throw error
+    }
+}
+
+/**
+ *
+ */
+void updatePlatformNet(Map envArgs = [:], Map clientDuArgs = [:], Map engineDuArgs = [:]) {
+    try{
+        def kbLibHelper = new PropertiesHelper()
+        // ----------------------------- Print Debug vars
+        echo "INFO generatedLanguage:: ${envArgs.generatedLanguage}"
+        echo "INFO dataSource:: ${envArgs.dataSource}"
+        envArgs.targetPath = "${envArgs.generatedLanguage}${envArgs.dataSource}"
+
     } catch (error) {
         currentBuild.result = 'FAILURE'
         throw error
