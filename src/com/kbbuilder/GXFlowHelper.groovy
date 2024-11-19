339181029ff1bb95b269d490e0d6ee15670847b0
Julián Albarellos <53489408+JulianAlbarellos@users.noreply.github.com>
2024-11-19 17:04:31 -0300

    Test full platform integration

--- src/com/kbbuilder/GXFlowHelper.groovy
+++ src/com/kbbuilder/GXFlowHelper.groovy
@@ -91,58 +91,58 @@ void updatePlatformJava(Map envArgs = [:], Map clientDuArgs = [:], Map engineDuA
         echo "INFO dataSource:: ${envArgs.dataSource}"
         echo "INFO targetPath:: ${envArgs.targetPath}"
         
-        // stage("Prepare ENV:${envArgs.targetPath}") {
-        //     kbLibHelper.setEnvironmentProperty(envArgs, "TargetPath", envArgs.targetPath) 
-        //     powershell script: """
-        //         \$ErrorActionPreference = 'Stop'
-        //         if (Test-Path -Path "${envArgs.localKBPath}\\${envArgs.targetPath}") { Remove-Item -Path "${envArgs.localKBPath}\\${envArgs.targetPath}" -Recurse -Force }
-        //         \$null = New-Item -Path "${envArgs.localKBPath}\\${envArgs.targetPath}\\web\\lib" -ItemType Directory
+        stage("Prepare ENV:${envArgs.targetPath}") {
+            kbLibHelper.setEnvironmentProperty(envArgs, "TargetPath", envArgs.targetPath) 
+            powershell script: """
+                \$ErrorActionPreference = 'Stop'
+                if (Test-Path -Path "${envArgs.localKBPath}\\${envArgs.targetPath}") { Remove-Item -Path "${envArgs.localKBPath}\\${envArgs.targetPath}" -Recurse -Force }
+                \$null = New-Item -Path "${envArgs.localKBPath}\\${envArgs.targetPath}\\web\\lib" -ItemType Directory
 
-        //         # --------------------- TODO: Update reference to circle dependency
-        //         Write-Output((Get-Date -Format G) + " [INFO] Sync ${WORKSPACE}\\Libs\\Java\\*")
-        //         Copy-Item -Path "${WORKSPACE}\\Libs\\Java\\*" -Destination "${envArgs.localKBPath}\\${envArgs.targetPath}\\web\\lib\\" -Force -Recurse
+                # --------------------- TODO: Update reference to circle dependency
+                Write-Output((Get-Date -Format G) + " [INFO] Sync ${WORKSPACE}\\Libs\\Java\\*")
+                Copy-Item -Path "${WORKSPACE}\\Libs\\Java\\*" -Destination "${envArgs.localKBPath}\\${envArgs.targetPath}\\web\\lib\\" -Force -Recurse
                 
-        //         # --------------------- Sync files from GeneXus Installation
-        //         Write-Output((Get-Date -Format G) + " [INFO] Sync ${envArgs.gxBasePath}\\Packages\\Gxpm\\RuleEvaluator\\Java\\com.gxflow.rules.jar")
-        //         Copy-Item -Path "${envArgs.gxBasePath}\\Packages\\Gxpm\\RuleEvaluator\\Java\\com.gxflow.rules.jar" -Destination "${envArgs.localKBPath}\\${envArgs.targetPath}\\web\\lib\\" -Force -Recurse
-        //         Write-Output((Get-Date -Format G) + " [INFO] Sync ${envArgs.gxBasePath}\\Packages\\Gxpm\\WFCache\\Java\\wfcache.jar")
-        //         Copy-Item -Path "${envArgs.gxBasePath}\\Packages\\Gxpm\\WFCache\\Java\\wfcache.jar" -Destination "${envArgs.localKBPath}\\${envArgs.targetPath}\\web\\lib\\" -Force -Recurse
-        //         Write-Output((Get-Date -Format G) + " [INFO] Extract JavaProt.zip")
-        //         Expand-Archive -Path "${envArgs.gxBasePath}\\Packages\\GXPM\\Protection\\JavaProt.zip" -DestinationPath "${envArgs.localKBPath}\\${envArgs.targetPath}\\web\\lib" -Force
+                # --------------------- Sync files from GeneXus Installation
+                Write-Output((Get-Date -Format G) + " [INFO] Sync ${envArgs.gxBasePath}\\Packages\\Gxpm\\RuleEvaluator\\Java\\com.gxflow.rules.jar")
+                Copy-Item -Path "${envArgs.gxBasePath}\\Packages\\Gxpm\\RuleEvaluator\\Java\\com.gxflow.rules.jar" -Destination "${envArgs.localKBPath}\\${envArgs.targetPath}\\web\\lib\\" -Force -Recurse
+                Write-Output((Get-Date -Format G) + " [INFO] Sync ${envArgs.gxBasePath}\\Packages\\Gxpm\\WFCache\\Java\\wfcache.jar")
+                Copy-Item -Path "${envArgs.gxBasePath}\\Packages\\Gxpm\\WFCache\\Java\\wfcache.jar" -Destination "${envArgs.localKBPath}\\${envArgs.targetPath}\\web\\lib\\" -Force -Recurse
+                Write-Output((Get-Date -Format G) + " [INFO] Extract JavaProt.zip")
+                Expand-Archive -Path "${envArgs.gxBasePath}\\Packages\\GXPM\\Protection\\JavaProt.zip" -DestinationPath "${envArgs.localKBPath}\\${envArgs.targetPath}\\web\\lib" -Force
                 
-        //         # --------------------- Create VirtualDirCreationDisabled file
-        //         Write-Output((Get-Date -Format G) + " [INFO] Create VirtualDirCreationDisabled file in modelDir")
-        //         if(-not (Test-Path -Path "${envArgs.localKBPath}\\${envArgs.targetPath}\\VirtualDirCreationDisabled")) {\$null = New-Item -Path "${envArgs.localKBPath}\\${envArgs.targetPath}\\VirtualDirCreationDisabled"}
-        //     """
-        // }
-        // stage("Build ENV:${envArgs.targetPath}") {
-        //     kbLibHelper.setEnvironmentProperty(envArgs, "translation_type", "Run-time")
-        //     kbLibHelper.setEnvironmentProperty(envArgs, "html_document_type", "HTML5")
-        //     kbLibHelper.setGeneratorProperty(envArgs, "Default", "java package name", "com.gxflow")
-        //     kbLibHelper.setGeneratorProperty(envArgs, "Default", "generate prompt programs", "No")
-        //     kbLibHelper.setGeneratorProperty(envArgs, "Default", "compiler_options", "-J-Xms1024m -J-Xmx2048m -O -source 1.8 -target 1.8")
-        //     kbLibHelper.setGeneratorProperty(envArgs, "Default", "java package name", "com.gxflow")
-        //     kbLibHelper.setGeneratorProperty(envArgs, "Default", "reorganization_options", "-nogui -noverifydatabaseschema -donotexecute")
-        //     kbLibHelper.setGeneratorProperty(envArgs, "Default", "Java platform support", "Both Platforms")
+                # --------------------- Create VirtualDirCreationDisabled file
+                Write-Output((Get-Date -Format G) + " [INFO] Create VirtualDirCreationDisabled file in modelDir")
+                if(-not (Test-Path -Path "${envArgs.localKBPath}\\${envArgs.targetPath}\\VirtualDirCreationDisabled")) {\$null = New-Item -Path "${envArgs.localKBPath}\\${envArgs.targetPath}\\VirtualDirCreationDisabled"}
+            """
+        }
+        stage("Build ENV:${envArgs.targetPath}") {
+            kbLibHelper.setEnvironmentProperty(envArgs, "translation_type", "Run-time")
+            kbLibHelper.setEnvironmentProperty(envArgs, "html_document_type", "HTML5")
+            kbLibHelper.setGeneratorProperty(envArgs, "Default", "java package name", "com.gxflow")
+            kbLibHelper.setGeneratorProperty(envArgs, "Default", "generate prompt programs", "No")
+            kbLibHelper.setGeneratorProperty(envArgs, "Default", "compiler_options", "-J-Xms1024m -J-Xmx2048m -O -source 1.8 -target 1.8")
+            kbLibHelper.setGeneratorProperty(envArgs, "Default", "java package name", "com.gxflow")
+            kbLibHelper.setGeneratorProperty(envArgs, "Default", "reorganization_options", "-nogui -noverifydatabaseschema -donotexecute")
+            kbLibHelper.setGeneratorProperty(envArgs, "Default", "Java platform support", "Both Platforms")
             
-        //     buildConfigurationEnvironment(envArgs)
+            buildConfigurationEnvironment(envArgs)
 
-        //     // //------------------ W.A for connection.gam
-        //     bat label: 'Create connection.gam',
-        //         script: "echo > \"${envArgs.localKBPath}\\${envArgs.targetPath}\\web\\connection.gam\""
-        // }
-        // stage("Make DB Schema Dynamic") {
-        //     bat script: """
-        //         "${envArgs.msbuildExePath}" "${envArgs.localKBPath}\\${envArgs.targetPath}\\Web\\${envArgs.msbuildDeployFile}" \
-        //         /p:GXInstall="${envArgs.gxBasePath}" \
-        //         /p:KBFolder="${envArgs.localKBPath}" \
-        //         /p:KBEnvironment="${envArgs.environmentName}" \
-        //         /p:TargetLanguage="${envArgs.generatedLanguage}" \
-        //         /p:TargetDbms="${envArgs.dataSource}" \
-        //         /p:KBEnvironmentPath="${envArgs.targetPath}" \
-        //         /t:MakeDBSchemaDynamic
-        //     """
-        // }
+            // //------------------ W.A for connection.gam
+            bat label: 'Create connection.gam',
+                script: "echo > \"${envArgs.localKBPath}\\${envArgs.targetPath}\\web\\connection.gam\""
+        }
+        stage("Make DB Schema Dynamic") {
+            bat script: """
+                "${envArgs.msbuildExePath}" "${envArgs.localKBPath}\\${envArgs.targetPath}\\Web\\${envArgs.msbuildDeployFile}" \
+                /p:GXInstall="${envArgs.gxBasePath}" \
+                /p:KBFolder="${envArgs.localKBPath}" \
+                /p:KBEnvironment="${envArgs.environmentName}" \
+                /p:TargetLanguage="${envArgs.generatedLanguage}" \
+                /p:TargetDbms="${envArgs.dataSource}" \
+                /p:KBEnvironmentPath="${envArgs.targetPath}" \
+                /t:MakeDBSchemaDynamic
+            """
+        }
         stage("Package ENV:${envArgs.targetPath} Platform") {
             // ----------------------------- Package Platform resources
             envArgs.deployTarget = "${envArgs.localKBPath}\\${envArgs.targetPath}\\Integration"
