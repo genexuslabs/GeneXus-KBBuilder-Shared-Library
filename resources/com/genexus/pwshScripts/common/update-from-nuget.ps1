daf212971df4d4a6cb90a5cfe5d0793f7db3ed1a
Julián Albarellos <53489408+JulianAlbarellos@users.noreply.github.com>
2024-11-29 11:37:46 -0300

    Fix

--- resources/com/genexus/pwshScripts/common/update-from-nuget.ps1
+++ resources/com/genexus/pwshScripts/common/update-from-nuget.ps1
@@ -45,7 +45,7 @@ try {
 }
 # Restore the project
 dotnet restore $DepsProjectPath
-$localPackagesPath = Join-Path -Path $LOCAL_NUGET_CACHE -ChildPath "$($PackageId.ToLower())\$PackageVersion\"
-$Package = Get-ChildItem -Path "$localPackagesPath\" -Filter ".zip"
+$localPackagesPath = Join-Path -Path $LOCAL_NUGET_CACHE -ChildPath "$($PackageId.ToLower())\$PackageVersion"
+$Package = Get-ChildItem -Path "$localPackagesPath" -Filter "*.zip"
 Write-Output "$(Get-Date -Format G) [DEBUG] Read downloaded package zip: $Package.Name"
 Invoke-Command -ScriptBlock {& "$PSScriptRoot\update-from-zip.ps1" (Join-Path -Path $localPackagesPath -ChildPath $Package.Name) $DeployTarget}
