6b8b37158dc98ce7d3e014e4213ca0a2c6fb915a
Joaquín Machado Vila <jmachado@genexus.com>
2025-05-02 17:24:59 -0300

    Extend packageDockerDU to also delete gxdprodj files, and added renameandcopysqlscript method

--- src/com/genexus/FlywayHelper.groovy
+++ src/com/genexus/FlywayHelper.groovy
@@ -49,4 +49,34 @@ void syncFlywayPackages(Map args = [:]) {
 }
 
 
+/**
+ * Renames and copies SQL reorganization script to a specified destination directory with a timestamp.
+ * The script is renamed following the pattern: V[timestamp]___[projectName]_ReorganizationScript_[BUILD_NUMBER].sql
+ *
+ * @param args A map containing the following parameters:
+ *   - sqlScriptDestinationDirectory: The destination directory where the SQL script will be copied.
+ *   - projectName: The name of the project to be included in the renamed file.
+ *   - reorgExportPath: The source directory containing the original reorganization script.
+ *   - BUILD_NUMBER: (Environment variable) The current build number used in the file naming.
+ *
+ * The method uses PowerShell to:
+ *   1. Generate a timestamp in format "yyyy.MM.dd.HHmmss"
+ *   2. Construct the destination path with the new file name
+ *   3. Copy the original SQL script to the destination if the destination directory exists
+ */
+void renameAndCopySQLScript(Map args = [:]) {
+    powershell script: """
+        \$ErrorActionPreference = "Stop"
+        \$currentDate = Get-Date -Format \"yyyy.MM.dd.HHmmss\" 
+
+        \$destinationPath = "${args.sqlScriptDestinationDirectory}\\V\$currentDate`___${args.projectName}_ReorganizationScript_${env.BUILD_NUMBER}.sql"
+        
+        \$originalSQLPath = "${args.reorgExportPath}\\${env.BUILD_NUMBER}_ReorganizationScript.txt"
+                                
+        if (Test-Path -Path ${args.sqlScriptDestinationDirectory}) {
+            Copy-Item -Path "\$originalSQLPath" -Destination "\$destinationPath" -Force
+        }
+    """
+}
+
 return this
