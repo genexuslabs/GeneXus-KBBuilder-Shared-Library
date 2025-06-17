5d8b3de955afcb5d6592af2a3bb007f6cff35645
Joaquín Machado Vila <jmachado@genexus.com>
2025-06-17 12:21:54 -0300

    Fixes gitPull method

--- src/com/genexus/GitHelper.groovy
+++ src/com/genexus/GitHelper.groovy
@@ -61,9 +61,14 @@ void commitUsingGitHubAppBuilderToken(Map args = [:]) {
 }
 
 void gitPull(Map args = [:]) {
-    powershell script: """
-        git pull origin ${args.gitBranch}
-    """
+    withCredentials([usernamePassword(credentialsId: "${projectDefinition.gitCredentialsId}", usernameVariable: 'GITHUB_APP', passwordVariable: 'GITHUB_ACCESS_TOKEN')]) {
+        def authenticatedUrl = projectDefinition.gitRepositoryUrl.replace("https://", "https://x-access-token:${GITHUB_ACCESS_TOKEN}@")
+        powershell script: """
+            git config --global credential.helper ""
+            git remote set-url origin '${authenticatedUrl}'
+            git pull origin ${projectDefinition.gitBranch}
+        """, returnStdout: true
+    }
 }
 
 void publishReorganizationScript(LinkedHashMap reorgPublishTypeDefinition, String reorgFullPath) {
